b4ebccfd893dbec448e50c630c43ab3effd311ca
musmanirfan <usmanmala356@gmail.com>
2024-08-28 02:39:01 +0500

    name change

--- script.js
+++ script.js
@@ -3,26 +3,34 @@ function convertFun() {
     const inputUnit = document.querySelector("#input-unit").value;
     const targetUnit = document.querySelector("#target-unit").value;
     const temperatureValue = parseFloat(document.querySelector("#temperature-value").value);
-    
+    let output = document.querySelector("#output");
+
     let convertedValue;
+    let arr = [];
 
     if (inputUnit === "Celsius" && targetUnit === "Fahrenheit") {
-        convertedValue = (temperatureValue * 9/5) + 32;
+        convertedValue = (temperatureValue * 9 / 5) + 32;
     } else if (inputUnit === "Celsius" && targetUnit === "Kelvin") {
         convertedValue = temperatureValue + 273.15;
     } else if (inputUnit === "Fahrenheit" && targetUnit === "Celsius") {
-        convertedValue = (temperatureValue - 32) * 5/9;
+        convertedValue = (temperatureValue - 32) * 5 / 9;
     } else if (inputUnit === "Fahrenheit" && targetUnit === "Kelvin") {
-        convertedValue = ((temperatureValue - 32) * 5/9) + 273.15;
+        convertedValue = ((temperatureValue - 32) * 5 / 9) + 273.15;
     } else if (inputUnit === "Kelvin" && targetUnit === "Celsius") {
         convertedValue = temperatureValue - 273.15;
     } else if (inputUnit === "Kelvin" && targetUnit === "Fahrenheit") {
-        convertedValue = ((temperatureValue - 273.15) * 9/5) + 32;
-    }else if(temperatureValue === ""){
+        convertedValue = ((temperatureValue - 273.15) * 9 / 5) + 32;
+    } else if (temperatureValue === "") {
         alert("plz inter value")
     } else {
         convertedValue = temperatureValue;
     }
 
-    document.querySelector("#output").innerText = `😗 ${convertedValue} ${targetUnit}`;
+    arr.push(`😗 ${convertedValue} ${targetUnit}`)
+    for (let i = 0; i < arr.length; i++) {
+
+        output.innerText += `😗 ${convertedValue} ${targetUnit}`;
+        console.log(i);
+    }
+
 };
